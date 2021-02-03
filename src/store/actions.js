6d1144b0c9e813ffc5d4da6fa6a7978b30878575
hajarghannami <hajarghannami@outlook.com>
2021-02-03 21:15:30 +0300

    updated actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,6 +1,8 @@
 // ACTION TYPES
 export const ADD_MEMBER = "ADD_MEMBER";
 export const ADD_BOOK = "ADD_BOOK";
+export const UPDATE_MEMBER = "UPDATE_MEMBER";
+export const UPDATE_BOOK = "UPDATE_BOOK";
 
 // Add Member Action
 export const addMember = (newMember) => {
@@ -15,5 +17,21 @@ export const addBook = (newBook) => {
   return {
     type: ADD_BOOK,
     payload: { newBook },
+  };
+};
+
+// Update Member Action
+export const updateMember = (updatedMember) => {
+  return {
+    type: UPDATE_MEMBER,
+    payload: { updatedMember },
+  };
+};
+
+// Update Book Action
+export const updateBook = (updatedBook) => {
+  return {
+    type: UPDATE_BOOK,
+    payload: { updatedBook },
   };
 };
